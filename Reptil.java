e46aa8cc65153d17c9951754088be0a3a25a570b
Gabito <101840148+gaturama@users.noreply.github.com>
2022-10-19 08:12:44 -0300

    adição de comandos

--- Reptil.java
+++ Reptil.java
@@ -1,3 +1,30 @@
-public class Reptil {
+import java.util.ArrayList;
+
+public class Reptil extends Animal{
     
-}
+   private String escama;
+    
+    public static ArrayList<Reptil> repteis = new ArrayList<Reptil>();
+    
+    public Reptil(int id, String nome, String especie, String escama) {
+        super(id, nome, especie);
+        this.setEscama(escama);
+        
+        repteis.add(this);
+    }
+    
+    public String getEscama(){
+        return escama;
+    }
+    public void setEscama(String escama) {
+        this.escama = escama;
+    }
+    
+    @Override
+    public String toString(){
+        return "ID: " + super.getId() + "\n"
+        + "Nome: " + super.getNome() + "\n" 
+        + "Espécie: " + super.getEspecie() + "\n"
+        + "Escama: " + this.Escama() + "\n";
+    }
+}    
